ed3728b580b67b980871cb08cf654fd0636b3223
psomareddy <prakash@cloudmasters.biz>
2025-03-17 21:28:31 -0400

    update

--- run-oc.sh
+++ run-oc.sh
@@ -9,4 +9,5 @@ export SPLUNK_MEMORY_TOTAL_MIB=512
 export SPLUNK_MEMORY_LIMIT_MIB=512
 export SPLUNK_BUNDLE_DIR=/usr/lib/splunk-otel-collector/agent-bundle
 export SPLUNK_COLLECTD_DIR=/usr/lib/splunk-otel-collector/agent-bundle/run/collectd
-./otelcol-dev/otelcol-dev --config config.yaml
+export SPLUNK_LISTEN_INTERFACE=0.0.0.0
+./otelcol-dev/otelcol-dev --config otelcol-config.yml
